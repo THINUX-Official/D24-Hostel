302337c35f5880ef8ed3d27342eaaea979bcb3e4
THINUX-Official <thinuxofficial@gmail.com>
2023-04-13 11:23:22 +0530

    Create Save, Update, Delete methods in RoomBOImpl and edti attributes in RoomFormController.

--- src/lk/ijse/d24/hostel/bo/custom/impl/RoomBOImpl.java
+++ src/lk/ijse/d24/hostel/bo/custom/impl/RoomBOImpl.java
@@ -12,15 +12,16 @@ import lk.ijse.d24.hostel.entity.Room;
 import lk.ijse.d24.hostel.model.RoomDTO;
 import lk.ijse.d24.hostel.util.SessionFactoryConfiguration;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RoomBOImpl implements RoomBO {
 
-    RoomDAO roomDAO = (RoomDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ROOMDAO);
-
     private Session session;
+
+    RoomDAO roomDAO = (RoomDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ROOMDAO);
 
     @Override
     public List<RoomDTO> getAllRoom() throws Exception {
@@ -29,7 +30,7 @@ public class RoomBOImpl implements RoomBO {
         List<Room> allRooms = roomDAO.getAll();
         ArrayList<RoomDTO> roomDTOS = new ArrayList<>();
 
-        for (Room room : allRooms){
+        for (Room room : allRooms) {
             roomDTOS.add(new RoomDTO(room.getRoomTypeId(), room.getType(), room.getKeyMoney(), room.getQty()));
         }
 
@@ -38,17 +39,59 @@ public class RoomBOImpl implements RoomBO {
 
     @Override
     public String saveRoom(RoomDTO roomDTO) throws Exception {
-        return null;
+        session = SessionFactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            roomDAO.setSession(session);
+            String id = roomDAO.save(new Room(roomDTO.getRoomTypeId(), roomDTO.getType(), roomDTO.getKeyMoney(), roomDTO.getQty()));
+            transaction.commit();
+            session.close();
+            return id;
+        } catch (Exception e) {
+            transaction.rollback();
+            session.close();
+            e.printStackTrace();
+            return "RES-001";
+        }
     }
 
     @Override
     public boolean updateRoom(RoomDTO roomDTO) throws Exception {
-        return false;
+        session = SessionFactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            roomDAO.setSession(session);
+            roomDAO.update(new Room(roomDTO.getRoomTypeId(), roomDTO.getType(), roomDTO.getKeyMoney(), roomDTO.getQty()));
+            transaction.commit();
+            session.close();
+            return true;
+        } catch (Exception e){
+            transaction.rollback();
+            session.close();
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean deleteRoom(RoomDTO roomDTO) throws Exception {
-        return false;
+        session = SessionFactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            roomDAO.setSession(session);
+            roomDAO.delete(new Room(roomDTO.getRoomTypeId(), roomDTO.getType(), roomDTO.getKeyMoney(), roomDTO.getQty()));
+            transaction.commit();
+            session.close();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            session.close();
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
--- src/lk/ijse/d24/hostel/controller/RoomFormController.java
+++ src/lk/ijse/d24/hostel/controller/RoomFormController.java
@@ -7,57 +7,32 @@ package lk.ijse.d24.hostel.controller;
 
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 import lk.ijse.d24.hostel.model.RoomDTO;
 
-import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
 public class RoomFormController {
-    @FXML
-    private AnchorPane pane;
-
-    @FXML
-    private JFXTextField txtRID;
-
-    @FXML
-    private JFXTextField txtType;
-
-    @FXML
-    private JFXTextField txtKeyMoney;
-
-    @FXML
-    private JFXTextField txtRoomQty;
-
-    @FXML
-    private JFXTextField txtSearch;
-
-    @FXML
-    private TableView<RoomDTO> tblRoom;
-
-    /*@FXML
-    private TableColumn<RoomDTO, String> colRID;
-
-    @FXML
-    private TableColumn<RoomDTO, String> colType;
-
-    @FXML
-    private TableColumn<RoomDTO, String> colKeyMoney;
-
-    @FXML
-    private TableColumn<RoomDTO, Integer> colRoomQty;*/
-
-    @FXML
-    private Label lblIID;
-
-    @FXML
-    private Label lblUnitPrice;
-
-    @FXML
-    private Label lblQtyOnHand;
+    public AnchorPane pane;
+    public JFXTextField txtRID;
+    public JFXTextField txtType;
+    public JFXTextField txtKeyMoney;
+    public JFXTextField txtRoomQty;
+    public JFXTextField txtSearch;
+    public TableView tblRoom;
+    public TableColumn colRID;
+    public TableColumn colType;
+    public TableColumn colKeyMoney;
+    public TableColumn colRoomQty;
+    public Label lblRID;
+    public Label lblKeyMoney;
+    public Label lblRoomQty;
+    public Label lblType;
 
     public void addOnAction(javafx.event.ActionEvent actionEvent) {
 
